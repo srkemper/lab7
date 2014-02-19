9781344a539e1f00411e1d9c3b52cc6cf801cd9a
Sean Kemper <srkemper@stanford.edu>
2014-02-19 23:30:43 +0000

    lab complete, starting stretch goal

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,7 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  models.Project.find({ "_id" : projectID}).exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -16,13 +16,27 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+
+  var project = new models.Project({
+    "title" : form_data.project_title,
+    "date" : form_data.date,
+    "summary" : form_data.summary,
+    "image" : form_data.image_url
+  });
+  project.save(function(err, project) {
+    if (err) {console.log(err); res.send(500);}
+    console.log(project);
+    res.redirect('/');
+  })
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
+  models.Project.find({ "_id" : projectID}).remove().exec( function (err, removed) {
+    res.send();
+  });
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
